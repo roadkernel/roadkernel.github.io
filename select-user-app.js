d5860e1dfad545c55000e7af0f1d7bb2c05bced4
kernel <160052192+roadkernel@users.noreply.github.com>
2024-05-23 20:04:49 +0300

    added several more checks for Samsung Defail Browser
    Holy fuck its retarded

--- select-user-app.js
+++ select-user-app.js
@@ -1,8 +1,7 @@
-if ((navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) || ('ontouchstart' in window || navigator.maxTouchPoints)) {
+if ((navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) || ('ontouchstart' in window || navigator.maxTouchPoints) || navigator.userAgent.match(/SamsungBrowser/i)) {
     var script = document.createElement('script');
     script.src = 'mobile-app.js';
     document.body.appendChild(script);
-    alert('Samsung Browser will break the app')
 } else {
     var script = document.createElement('script');
     script.src = 'app.js';
